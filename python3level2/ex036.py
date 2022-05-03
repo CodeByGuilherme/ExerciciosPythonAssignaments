222c46f9a7fbf0fd4ee6240dc3a3cf9d6d0e2fc7
Guilherme Teixeira <54123588+xGuilhermeTx@users.noreply.github.com>
2022-05-02 21:52:12 -0300

    Update ex036.py

--- python3level2/ex036.py
+++ python3level2/ex036.py
@@ -1,7 +1,19 @@
 '''
-Escreva um programa para aprovar o empréstimo bancário para a compa de uma casa.
+Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
 O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
-Calcule ovalorda prestação mensal, sabendo  que ela não pode exceder 30% salário,
+Calcule o valor da prestação mensal, sabendo  que ela não pode exceder 30% salário,
 ou então, o empréstimo será negado.
 '''
+
+salvalor = float(input('Digite o salário do solicitante: '))
+imovelvalor = float(input('Digite o valor do imóvel: '))
+anosfinanc = int(input('Digite em quantos anos vocẽ pretende financiar: '))
+parcelavalor = imovelvalor/anosfinanc/12
+print('o valor da sua parcelá sairá : {:.2f}'.format(parcelavalor))
+tetoparcel  = salvalor/100*30
+if parcelavalor<=tetoparcel:
+    print('O empréstimo foi aprovado')
+else:
+    print('O empréstimo foi negado')
+
 
